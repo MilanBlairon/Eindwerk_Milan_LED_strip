63f1d200f667db10ca263c211f6fe95dfd52e80e
Milan Blairon <milan.blairon@student.vives.be>
2023-12-05 17:17:09 +0100

    Knoppen voor kleurmodus toegevoegd en werkend gesteld. Knoppen veranderen van kleur indien ze geselecteerd zijn. Data versturen via seriële poort zou nu ook naar behoren moeten werken (gecontroleerd met Putty). Deze WPF zou naar behoren moeten werken, enkel Arduino code nog doen werken hiermee.
    Later meer kleurmodusknoppen maken en GUI verder uitbreiden.

--- Eindwerk_Milan_LED_strip/MainWindow.xaml.cs
+++ Eindwerk_Milan_LED_strip/MainWindow.xaml.cs
@@ -20,7 +20,7 @@ namespace Eindwerk_Milan_LED_strip
 {
     public partial class MainWindow : Window
     {
-        SerialPort _serialPort;
+        private SerialPort _serialPort;
 
         public MainWindow()
         {
@@ -28,78 +28,182 @@ namespace Eindwerk_Milan_LED_strip
 
             _serialPort = new SerialPort();
 
+            // Voeg "None" toe aan de combobox
             cbxComPorts.Items.Add("None");
-            foreach (string s in SerialPort.GetPortNames())
-                cbxComPorts.Items.Add(s);
+
+            // Voeg beschikbare poorten toe aan de combobox
+            foreach (string portName in SerialPort.GetPortNames())
+            {
+                cbxComPorts.Items.Add(portName);
+            }
         }
 
         private void cbxComPorts_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
         {
-            if (_serialPort != null)
+            try
             {
-                if (_serialPort.IsOpen)
+                if (_serialPort != null)
                 {
-                    _serialPort.Close();
-                }
+                    if (_serialPort.IsOpen)
+                    {
+                        _serialPort.Close();
+                    }
 
-                if (cbxComPorts.SelectedItem.ToString() != "None")
-                {
-                    _serialPort.PortName = cbxComPorts.SelectedItem.ToString();
-                    _serialPort.Open();
-                    sldrBirghtness.IsEnabled = true;
+                    // Controleer op null voordat ToString() wordt aangeroepen
+                    if (cbxComPorts.SelectedItem != null && cbxComPorts.SelectedItem.ToString() != "None")
+                    {
+                        _serialPort.PortName = cbxComPorts.SelectedItem.ToString();
+                        _serialPort.Open();
+                        sldrBrightness.IsEnabled = true;
+                    }
+                    else
+                    {
+                        sldrBrightness.IsEnabled = false;
+                    }
                 }
-                else
-                    sldrBirghtness.IsEnabled = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Fout: " + ex.Message, "Fout", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
 
-        private void sldrBirghtness_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        private void sldrBrightness_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            byte data = 0;
-
-            for (int i = 0; i < sldrBirghtness.Value; i++)
+            try
             {
-                data <<= 1;
-                data++;
+                // Stuur de schuifregelaarwaarde naar Arduino wanneer deze verandert
+                int value = (int)sldrBrightness.Value;
+                SendCommand("B" + value.ToString());
             }
-
-            if ((_serialPort != null) && (_serialPort.IsOpen))
+            catch (Exception ex)
             {
-                _serialPort.Write(new byte[] { data }, 0, 1);
+                MessageBox.Show("Fout: " + ex.Message, "Fout", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
 
         private void btnSetWhiteColor_Click(object sender, RoutedEventArgs e)
         {
-            SendCommand("W"); // Replace "W" with the command you want to send for setWhiteColor
+            SendCommand("W");
+            KnoppenTransparant();
+            WitAchtergrond();   
+        }
+
+        private void WitAchtergrond()
+        {
+            LinearGradientBrush gradientBrush = new LinearGradientBrush();
+            gradientBrush.StartPoint = new Point(0, 0);
+            gradientBrush.EndPoint = new Point(1, 1);
+
+            // Omgekeerde volgorde
+            gradientBrush.GradientStops.Add(new GradientStop(Color.FromRgb(200, 200, 200), 0)); // Donkerder
+            gradientBrush.GradientStops.Add(new GradientStop(Color.FromRgb(255, 255, 255), 1)); // Witter
+
+
+
+
+            btnWit.Background = gradientBrush;
         }
 
         private void btnRainbowCycle_Click(object sender, RoutedEventArgs e)
         {
-            SendCommand("R"); // Replace "R" with the command you want to send for rainbowCycle
+            SendCommand("R");
+            KnoppenTransparant();
+            RegenboogAchtergrond();
+
+        }
+
+        private void RegenboogAchtergrond()
+        {
+            LinearGradientBrush gradientBrush = new LinearGradientBrush();
+            gradientBrush.StartPoint = new Point(0, 0);
+            gradientBrush.EndPoint = new Point(1, 1);
+
+            gradientBrush.GradientStops.Add(new GradientStop(Colors.Red, 0));
+            gradientBrush.GradientStops.Add(new GradientStop(Colors.Orange, 0.17));
+            gradientBrush.GradientStops.Add(new GradientStop(Colors.Yellow, 0.33));
+            gradientBrush.GradientStops.Add(new GradientStop(Colors.Green, 0.5));
+            gradientBrush.GradientStops.Add(new GradientStop(Colors.Blue, 0.67));
+            gradientBrush.GradientStops.Add(new GradientStop(Colors.Indigo, 0.83));
+            gradientBrush.GradientStops.Add(new GradientStop(Colors.Violet, 1));
+
+            btnRegenboog.Background = gradientBrush;
+        }
+
+        private void KnoppenTransparant()
+        {
+            btnRegenboog.Background = new SolidColorBrush(Colors.Transparent);
+            btnWit.Background = new SolidColorBrush(Colors.Transparent);
         }
 
         private void SendCommand(string command)
         {
-            if ((_serialPort != null) && (_serialPort.IsOpen))
+            try
             {
-                _serialPort.Write(command);
+                if (_serialPort != null && _serialPort.IsOpen)
+                {
+                    _serialPort.Write(command + "\n\r");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Fout: " + ex.Message, "Fout", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            if ((_serialPort != null) && _serialPort.IsOpen)
+            try
             {
-                _serialPort.Write(new byte[] { 0 }, 0, 1);
-                _serialPort.Dispose();
+                if (_serialPort != null && _serialPort.IsOpen)
+                {
+                    // Stuur een byte om de Arduino te informeren dat de communicatie wordt beëindigd
+                    _serialPort.Write(new byte[] { 0 }, 0, 1);
+                    _serialPort.Close();
+                    _serialPort.Dispose();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Fout bij het sluiten: " + ex.Message, "Fout", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
 
+
+
         private void btnOnOff_Click(object sender, RoutedEventArgs e)
         {
+            // Toggle de kleur van de knop en de tekst
+            AanUitKnop();
+        }
+
+
+        private void AanUitKnop()
+        {
+            // Toggle de kleur van de knop
+            if (btnOnOff.Background is SolidColorBrush solidColorBrush && solidColorBrush.Color == Colors.Red)
+            {
+                LinearGradientBrush gradientBrush = new LinearGradientBrush();
+                gradientBrush.StartPoint = new Point(0, 0);
+                gradientBrush.EndPoint = new Point(1, 1);
+
+                gradientBrush.GradientStops.Add(new GradientStop(Color.FromRgb(0, 128, 0), 0)); // Donkergroen
+                gradientBrush.GradientStops.Add(new GradientStop(Color.FromRgb(0, 255, 0), 1)); // Helder groen
+
+                btnOnOff.Background = gradientBrush;
+                btnOnOff.Content = "Ingeschakeld";
+                SendCommand("I"); // Ingeschakeld sturen
+
+            }
+            else
+            {
+                btnOnOff.Background = new SolidColorBrush(Colors.Red);
+                btnOnOff.Content = "Uitgeschakeld";
+                SendCommand("U"); // Uitgeschakeld sturen
+            }
 
         }
     }
 }
+
 
